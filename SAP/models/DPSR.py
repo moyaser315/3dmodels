22699ccef7dbcae7868f92425b429f93345ad357
troopmarshall <mohamedyaser.315@gmail.com>
2024-11-30 18:44:31 +0200

    finish DPSR

--- SAP/models/DPSR.py
+++ SAP/models/DPSR.py
@@ -2,39 +2,42 @@ import torch
 from torch import nn
 from ..utils import utils
 
+
 class DPSR(nn.Module):
     def __init__(self, res_grid, sigma):
-        super(DPSR,self).__init__()
+        super(DPSR, self).__init__()
         self.res_grid = res_grid
         self.sigma = sigma
         self.m = len(res_grid)
         self.freqs = utils.get_fft(res_grid)
-        self.G = utils.get_smoothed_guassian(self.freqs,self.res,sigma)
+        self.G = utils.get_smoothed_guassian(self.freqs, self.res, sigma)
 
-    def forward(self, points , normals):
+    def forward(self, points, normals):
 
-        v = None #TODO imp reasterization 
-        batch,dim,x,y,z = 0,1,2,3,4
-        # divergence (first drevitive) : 
+        v = None  # TODO imp reasterization
+        batch, dim, x, y, z = 0, 1, 2, 3, 4
+        # divergence (first drevitive) :
 
-        #prepering vector V to be computed in frequency domaibn
-        dv = torch.fft.rfft(v,dim=(x,y,z))
-        dv = dv.permute([batch,x,y,z,dim])
+        # prepering vector V to be computed in frequency domaibn
+        dv = torch.fft.rfft(v, dim=(x, y, z))
+        dv = dv.permute([batch, x, y, z, dim])
         dv = dv.unsqueeze(-1) * self.G
 
         freqs = self.freqs.unsqueeze(-1)
-        freqs *= (2*torch.pi)
+        freqs *= 2 * torch.pi
         freqs = freqs.to(points.device)
 
-        #computing dv
-        dv = torch.sum(torch.view_as_real(dv.unsqueeze(-1)) * freqs , dim=-2) #TODO:handle complex numbers
+        # computing dv
+        dv = torch.sum(
+            -utils.get_img_sign(torch.view_as_real(dv[..., 0])) * freqs, dim=-2
+        )  # TODO:handle complex numbers
 
-        #Laplacian (second derivative) :
-        lap = -torch.sum()
+        # Laplacian (second derivative) :
+        lap = -torch.sum(freqs**2, dim=-2)  # x^2 + y^2 + z^2
+        lap = dv / (lap + 1e-6)
+        # NOTE: in the original implementation there's a permuate that i dopn't know why it's there
+        lap = torch.fft.irfft(
+            torch.view_as_complex(lap), s=self.res_grid, dim=(1, 2, 3)
+        )
 
-
-
-
-        
-        
-
+        return lap
--- SAP/utils/utils.py
+++ SAP/utils/utils.py
@@ -1,20 +1,39 @@
-import torch 
+import torch
 from typing import Tuple
 
-def get_fft(res_grid : tuple) ->torch.tensor :
+
+def get_fft(res_grid: tuple) -> torch.tensor:
     freqs = []
-    for res in res_grid :
-        freqs.append(torch.fft.fftfreq(res,1/res))
+    for res in res_grid:
+        freqs.append(torch.fft.fftfreq(res, 1 / res))
     freqs = torch.meshgrid(freqs)
     freqs = list(freqs)
-    freqs = torch.stack(freqs,dim=-1)
+    freqs = torch.stack(freqs, dim=-1)
     return freqs
 
-def get_smoothed_guassian(freqs,res, sigma):
 
-    ecludian_dis = torch.sqrt(torch.sum(freqs ** 2, dim=-1))
-    apply_filter = torch.exp(-0.5*((sigma*2*ecludian_dis/res[0])**2))
-    apply_filter = apply_filter.unsqueeze(-1).unsqueeze(-1) #for brodcasting from the original code
+def get_smoothed_guassian(freqs, res, sigma):
+
+    ecludian_dis = torch.sqrt(torch.sum(freqs**2, dim=-1))
+    apply_filter = torch.exp(-0.5 * ((sigma * 2 * ecludian_dis / res[0]) ** 2))
+    apply_filter = apply_filter.unsqueeze(-1).unsqueeze(
+        -1
+    )  # for brodcasting from the original code
     apply_filter.requires_grad = False
 
     return apply_filter
+
+
+def get_img_sign(x, deg=1):
+    deg %= 4
+    if deg == 0:
+        res = x
+    elif deg == 1:
+        res = x[..., [1, 0]]
+        res[..., 0] = -res[..., 0]
+    elif deg == 2:
+        res = -x
+    elif deg == 3:
+        res = x[..., [1, 0]]
+        res[..., 1] = -res[..., 1]
+    return res
